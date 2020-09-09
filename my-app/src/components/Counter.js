cb40472a9f4ff8b9b24b02275451844fbe9f5259
Eric WH Huang <vuarnet0318@gmail.com>
2020-09-10 07:53:45 +0800

    day7: React_Hook=> useState, demo1-useState

--- my-app/src/components/Counter.js
+++ my-app/src/components/Counter.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 // *
 // 設計一個Counter，包含以下三個元件
@@ -6,14 +6,14 @@ import React from "react";
 // <Button1 /> <Button2 />
 // *
 export default function Counter() {
-  let numState = 5; //<-- 給定一個預設值5
+  const [numState, setNumState] = useState(5); //<-- 給定一個預設值5
   return (
     <>
       <h1>{numState}</h1>            
       <button
         onClick={() => {
           console.log("Counter+1");
-          return numState + 1;
+          return setNumState(numState + 1);
         }}
       >
         加1
@@ -22,7 +22,7 @@ export default function Counter() {
       <button
         onClick={() => {
           console.log("Counter-1");
-          return numState - 1;
+          return setNumState(numState - 1);
         }}
       >
         減1
