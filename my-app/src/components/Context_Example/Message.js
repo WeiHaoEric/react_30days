93f10d57ef43bcb3654afd0312b014bd665e3826
Eric WH Huang <vuarnet0318@gmail.com>
2020-09-15 16:22:49 +0800

    day12: context demo2 by adding title with  boolean expression

--- my-app/src/components/Context_Example/Message.js
+++ my-app/src/components/Context_Example/Message.js
@@ -3,12 +3,13 @@ import Input from "./Input";
 export const MsgStore = createContext(); //<--在這裡建立一個context物件，並export出去，預備給其他component使用
 
 export default function Message() {
-  const [titleText, setTitleText] = useState("React Rookie's 30 days"); //<--useState設定
+	const initTitle = "React Rookie's 30 days";
+  const [titleText, setTitleText] = useState(initTitle); //<--useState設定
   return (
     <MsgStore.Provider
       value={{ titleState: titleText, titleFunc: setTitleText }}
     >
-      <h2>{titleText}</h2>
+      <h2>{titleText?titleText:setTitleText(initTitle)}</h2>
       <Input />
     </MsgStore.Provider>
   );
