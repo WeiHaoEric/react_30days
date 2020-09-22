ff736cf809a02ab7888d0b76a7b8c52e81143489
Eric WH Huang <vuarnet0318@gmail.com>
2020-09-22 11:21:35 +0800

    fix: correct import path in index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ToDoList from "./components/day16_ToDoApp/ToDoApp"
+import ToDoList from "./components/day18_ToDoApp/ToDoApp"
 
 ReactDOM.render(
   <ToDoList/>,
